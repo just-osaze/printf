e63a7285c8bbab24380bbf6177fac822f97b835a
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-06-18 08:59:39 -0700

    Osas and Josepj project commit

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN
 #define MAIN
 
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -52,7 +53,7 @@ int prinsint(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
-int print_buf(char *buf, unsigned int nbuf);
+int _putchar(char *buf, unsigned int nbuf);
 char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
 char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
